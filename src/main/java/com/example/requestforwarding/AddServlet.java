500f53c2ace020f98dee46b255213ebb14a7e68f
Karthik <karthi.rs.4631@gmail.com>
2025-09-30 11:08:09 +0530

    first commit

---
+++ src/main/java/com/example/requestforwarding/AddServlet.java
@@ -0,0 +1,26 @@
+package com.example.requestforwarding;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class AddServlet extends HttpServlet {
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int i = Integer.parseInt(req.getParameter("num1"));
+        int j = Integer.parseInt(req.getParameter("num2"));
+
+        int k = i+j;
+
+        req.setAttribute("k",k);
+        RequestDispatcher rd = req.getRequestDispatcher("sq");
+        rd.forward(req,resp);
+//        PrintWriter out = resp.getWriter();
+//        out.println("<h2>sum is: "+k+"</h2>");
+
+    }
+}
---
+++ src/main/java/com/example/requestforwarding/SqServlet.java
@@ -0,0 +1,18 @@
+package com.example.requestforwarding;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class SqServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int k =  (int) request.getAttribute("k");
+        PrintWriter out = response.getWriter();
+        out.println(k*k);
+
+    }
+}
