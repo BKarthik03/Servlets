5404689e4252168b5bf02776560b473e0826fb42
Karthik <karthi.rs.4631@gmail.com>
2025-10-04 15:34:15 +0530

    HttpSession

--- src/main/java/com/example/requestforwarding/AddServlet.java
+++ src/main/java/com/example/requestforwarding/AddServlet.java
@@ -5,22 +5,23 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 
 public class AddServlet extends HttpServlet {
-    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         int i = Integer.parseInt(req.getParameter("num1"));
         int j = Integer.parseInt(req.getParameter("num2"));
 
         int k = i+j;
 
-        req.setAttribute("k",k);
-        RequestDispatcher rd = req.getRequestDispatcher("sq");
-        rd.forward(req,resp);
-//        PrintWriter out = resp.getWriter();
-//        out.println("<h2>sum is: "+k+"</h2>");
+        HttpSession session = req.getSession();
+        session.setAttribute("k",k);
+
+        resp.sendRedirect("sq");
+
 
     }
 }
--- src/main/java/com/example/requestforwarding/SqServlet.java
+++ src/main/java/com/example/requestforwarding/SqServlet.java
@@ -4,15 +4,17 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 
 public class SqServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        int k =  (int) request.getAttribute("k");
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        int k =  (int) session.getAttribute("k");
         PrintWriter out = response.getWriter();
-        out.println(k*k);
+        out.println("<h2>Square of sum is: "+k*k);
 
     }
 }
