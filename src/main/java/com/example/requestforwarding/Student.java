5b3d8cb4eb3004190b35261407052828e206547c
Karthik <karthi.rs.4631@gmail.com>
2025-10-10 21:33:41 +0530

    SQL

---
+++ src/main/java/com/example/requestforwarding/Student.java
@@ -0,0 +1,31 @@
+package com.example.requestforwarding;
+
+public class Student {
+    int rollNo;
+    String name;
+
+    Student(int rollNo, String name) {
+        this.rollNo = rollNo;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getRollNo() {
+        return rollNo;
+    }
+
+    public void setRollNo(int rollNo) {
+        this.rollNo = rollNo;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "rollNo=" + rollNo +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
